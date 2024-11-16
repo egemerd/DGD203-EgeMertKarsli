ed2fe77cf325a141565d75c2a5787df2a1559234
egemerd <egemertkarsli@hotmail.com>
2024-11-16 19:04:41 +0300

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Item
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+
+        public Item(string aName, int aValue)
+        {
+            name = aName;
+            value = aValue;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Value: {value}";
+        }
+
+
+    }
+}
